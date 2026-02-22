e31b432bc8403b0ff7f2919d11eae88d67fa1642
Dean Valentine <dean@zeropath.com>
2026-02-22 12:34:37 -0800

    test(api): satisfy lint in root route unit test

--- src/typescript/api/test/unit/root-route.test.ts
+++ src/typescript/api/test/unit/root-route.test.ts
@@ -2,14 +2,11 @@ import assert from "node:assert/strict";
 import { test } from "node:test";
 import { GET } from "../../src/routes/+server.js";
 
-function createRequestEvent(): Parameters<typeof GET>[0] {
-  return {
-    request: new Request("http://localhost/"),
-  } as Parameters<typeof GET>[0];
-}
-
 test("root route returns an OpenAI-style JSON welcome message", async () => {
-  const response = await GET(createRequestEvent());
+  const routeGet = GET as (event: { request: Request }) => Promise<Response>;
+  const response = await routeGet({
+    request: new Request("http://localhost/"),
+  });
 
   assert.equal(response.status, 200);
   assert.equal(response.headers.get("content-type"), "application/json");
